e8c511456a4800e347441ac175f6ddeae90975dd
Imteyaz_king <79402181+Imteyazking@users.noreply.github.com>
2021-06-11 21:29:59 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,6 +1,6 @@
 from userbot import darkdef
 from ..helper import functions as dcdef
 #USELESS
-darkmusic = darkdef.darkmusic 
-darkmusicvideo = darkdef.darkmusicvideo
+Kingmusic = kingdef.darkmusic 
+Kingmusicvideo = kingdef.kingmusicvideo
 
